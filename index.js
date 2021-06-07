2cd439e61b13da6543ddfd94d231f65df2876075
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-06-06 23:13:25 -0400

    got the prompts to work together

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
 const inquirer = require('inquirer')
 
+
 let team = [];
 
-whoisManager = () => {
+whoIsManager = () => {
     inquirer
     .prompt([
         {
@@ -30,16 +31,109 @@ whoisManager = () => {
         name: 'officeNumber'
     },
     ])
-    .then((response) =>     
+    .then((response) =>   {
+    team.push( new Manager(response.nameManager, response.idManager, response.email, response.officeNumber));
+    console.log(team)
+    nextPerson();
+
     
-    console.log(response)
-    
-    
-    )
-  
+})
+.catch(function(error) {
+    console.log("AHHHHHH:", error); 
+
+    process.exit(1)
+})
 }
 
+    whoIsEngineer = () => {
+        inquirer
+        .prompt([
+            {
+            type: 'input',
+            message: 'What is the name of the engineer?',
+            name: 'nameEngineer'
+        },
+        {
+            type: 'input',
+            message: 'What is the employee ID of the engineer?',
+            name: 'idEngineer'
+        },
+        {
+            type: 'input',
+            message: 'What is the email of the engineer?',
+            name: 'email'
+        },        
+        {
+            type: 'input',
+            message: 'What is the GitHub of the engineer?',
+            name: 'gitHub'
+        },
+        ])
+        .then((response) =>   {
+        team.push( new Engineer(response.nameEngineer, response.idEngineer, response.email, response.gitHub));
+        console.log(team)
+        nextPerson();
+    }
+    
+        )}
+
+    whoIsIntern = () => {
+        inquirer
+        .prompt([
+            {
+            type: 'input',
+            message: 'What is the name of the Intern?',
+            name: 'nameIntern'
+        },
+        {
+            type: 'input',
+            message: 'What is the employee ID of the Intern?',
+            name: 'idIntern'
+        },
+        {
+            type: 'input',
+            message: 'What is the email of the Intern?',
+            name: 'email'
+        },        
+        {
+            type: 'input',
+            message: 'What is the School of the Intern?',
+            name: 'school'
+        },
+        ])
+        .then((response) =>   {
+        team.push( new Intern(response.nameIntern, response.idIntern, response.email, response.school));
+        console.log(team)
+        nextPerson();
+    }
+    
+        )}
+
+nextPerson = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'employee',
+            message: 'Which class of employee would you like to add next?',
+            choices: ['Engineer', 'Intern', 'No one else to add'],
+        }])
+        .then((response) => {
+            let answer = response.employee
+            if (answer === 'Engineer' ) {
+                whoIsEngineer();
+            } else if (answer === 'Intern' ) {
+                whoIsIntern();
+            } else {
+                console.log(`end: end function goes here`)
+                
+            }
+        })
+
+}
     
 
 
-whoisManager();
+whoIsManager();
+// whoIsEngineer();
+// nextPerson();

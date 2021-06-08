af2e9680daa781d6ce4978b2dbf367ad61ecce3b
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-06-08 15:52:14 -0400

    Intern Tests passed

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,36 @@
+const { expect } = require('@jest/globals')
+const Intern = require('../lib/intern')
+
+const testIntern = new Intern("Jon Nance", "10022079", 'jonathan.w.nance@gmail.com', 'NCSU')
+
+test ('Does getName() return the proper name positive flow', () => {
+    const test = 'NCSU'
+    
+    
+    expect(testIntern.getSchool()).toBe(test);
+    
+})
+
+test ('Does getName() return the proper name negative flow', () => {
+    const test = 'UNCW'
+    
+    
+ 
+    expect(testIntern.getSchool()).not.toBe(test);
+})
+
+test ('Does getRole() return the proper name positive flow', () => {
+    const test = 'Intern'
+    
+    
+    expect(testIntern.getRole()).toBe(test);
+    
+})
+
+test ('Does getRole() return the proper name negative flow', () => {
+    const test = 'Employee'
+    
+    
+ 
+    expect(testIntern.getRole()).not.toBe(test);
+})

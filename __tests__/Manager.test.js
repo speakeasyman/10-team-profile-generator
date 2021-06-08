1f0c4a9d5619489b5cfcb6eb1de6be778fbe98db
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-06-08 15:44:33 -0400

    Manager test cases completed

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,36 @@
+const { expect } = require('@jest/globals')
+const Manager = require('../lib/manager')
+
+const testManager = new Manager("Jon Nance", "10022079", 'jonathan.w.nance@gmail.com', '709')
+
+test ('Does getName() return the proper name positive flow', () => {
+    const test = '709'
+    
+    
+    expect(testManager.getOffice()).toBe(test);
+    
+})
+
+test ('Does getName() return the proper name negative flow', () => {
+    const test = '736C'
+    
+    
+ 
+    expect(testManager.getOffice()).not.toBe(test);
+})
+
+test ('Does getRole() return the proper name positive flow', () => {
+    const test = 'Manager'
+    
+    
+    expect(testManager.getRole()).toBe(test);
+    
+})
+
+test ('Does getRole() return the proper name negative flow', () => {
+    const test = 'Employee'
+    
+    
+ 
+    expect(testManager.getRole()).not.toBe(test);
+})
